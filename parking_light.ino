d4f62888de0b382896f2f407c6a0629e1b0dec31
DasonTiovino <codingdec14@gmail.com>
2024-04-19 14:21:51 +0700

    feat: create motorbike led and car led

--- parking_light.ino
+++ parking_light.ino
@@ -1,36 +1,76 @@
-const int yellowLedPin = 23;
-const int redLedPin = 32;
+const int motorbikeRedLed = 21;
+const int motorbikeGreenLed = 22;
+const int carRedLed = 12;
+const int carGreenLed = 13;
 
-// bool commandReceived = false;
-// unsigned long lastCommandTime = 0;
-// const unsigned long commandTimeout = 5000; // Timeout in milliseconds (adjust as needed)
+bool commandReceived = false;
+unsigned long lastCommandTime = 0;
+const unsigned long commandTimeout = 5000; // Timeout in milliseconds (adjust as needed)
 
 void setup() {
-  pinMode(redLedPin, OUTPUT);
-  pinMode(yellowLedPin, OUTPUT);
+  pinMode(motorbikeRedLed, OUTPUT);
+  pinMode(motorbikeGreenLed, OUTPUT);
+  pinMode(carRedLed, OUTPUT);
+  pinMode(carGreenLed, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  // if (Serial.available() > 0) {
+  //   char command = Serial.read();
+  //   if (command == 'Y') {
+  //     digitalWrite(motorbikeGreenLed, HIGH); // Turn on yellow LED
+  //     digitalWrite(motorbikeRedLed, LOW);
+  //     commandReceived = true;
+  //     lastCommandTime = millis(); // Update last command time
+  //   } else if (command == 'R') {
+  //     digitalWrite(motorbikeGreenLed, LOW);  // Turn off yellow LED
+  //     digitalWrite(motorbikeRedLed, HIGH); // Turn on red LED
+  //     commandReceived = true;
+  //     lastCommandTime = millis(); // Update last command time
+  //   }
+  // }
+
   if (Serial.available() > 0) {
     char command = Serial.read();
-    if (command == 'Y') {
-      digitalWrite(yellowLedPin, HIGH); // Turn on yellow LED
-      digitalWrite(redLedPin, LOW);
+    if (command == 'G'){
+      digitalWrite(motorbikeGreenLed, HIGH);
+      digitalWrite(carGreenLed, HIGH);
+      digitalWrite(carRedLed, LOW);
+      digitalWrite(motorbikeRedLed, LOW);
       commandReceived = true;
-      lastCommandTime = millis(); // Update last command time
+      lastCommandTime = millis();
     } else if (command == 'R') {
-      digitalWrite(yellowLedPin, LOW);  // Turn off yellow LED
-      digitalWrite(redLedPin, HIGH); // Turn on red LED
+      digitalWrite(motorbikeRedLed, HIGH);
+      digitalWrite(carRedLed, HIGH);
+      digitalWrite(motorbikeGreenLed, LOW);
+      digitalWrite(carGreenLed, LOW);
       commandReceived = true;
-      lastCommandTime = millis(); // Update last command time
+      lastCommandTime = millis();
+    } else if (command == 'M') {
+      digitalWrite(motorbikeGreenLed, HIGH);
+      digitalWrite(motorbikeRedLed, LOW);
+      digitalWrite(carGreenLed, LOW);
+      digitalWrite(carRedLed, HIGH);
+      commandReceived = true;
+      lastCommandTime = millis();
+    } else if (command == 'C') {
+      digitalWrite(motorbikeGreenLed, LOW);
+      digitalWrite(motorbikeRedLed, HIGH);
+      digitalWrite(carGreenLed, HIGH);
+      digitalWrite(carRedLed, LOW);
+      commandReceived = true;
+      lastCommandTime = millis();
     }
   }
 
-//   // Check if it's been too long since last command received
-//   if (commandReceived && (millis() - lastCommandTime) > commandTimeout) {
-//     digitalWrite(yellowLedPin, LOW);  // Turn off yellow LED
-//     digitalWrite(redLedPin, LOW); // Turn off red LED
-//     commandReceived = false; // Reset flag
-//   }
-// }
+  // Check if it's been too long since last command received
+  if (commandReceived && (millis() - lastCommandTime) > commandTimeout) {
+    digitalWrite(motorbikeGreenLed, LOW);  // Turn off motorbike green led
+    digitalWrite(motorbikeRedLed, LOW); // Turn off motorbike red led
+    digitalWrite(carGreenLed, LOW); // Turn off car green led
+    digitalWrite(carRedLed, LOW); // Turn off car red led
+    commandReceived = false; // Reset flag
+  }
+}
+
